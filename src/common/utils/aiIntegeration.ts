99f35d18d2ce03713c109fcb862e066df4e237bf
ziad <ziaddash33@gmail.com>
2025-07-03 19:19:37 +0300

    fix Authorization of axios issue

--- src/common/utils/aiIntegeration.ts
+++ src/common/utils/aiIntegeration.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: './config/.env' });
+
+const Authorization = `Bearer ${process.env.OPENROUTER_API_KEY}`;
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 export const openRouter = axios.create({
   baseURL: 'https://openrouter.ai/api/v1',
   headers: {
-    Authorization: `Bearer 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`,
+    Authorization,
     'Content-Type': 'application/json',
     'HTTP-Referer': 'http://localhost:3000',
   },
